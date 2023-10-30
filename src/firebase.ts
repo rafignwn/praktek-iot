c37504b9280276c50e014a73b3575d21d8d6ceca
Rafi gunawan <figun746@gmail.com>
2023-10-30 09:10:21 +0700

    update friebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,15 +5,26 @@ import { getDatabase } from "firebase/database";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAJh78Z3LBT9ob6dkipt64OhZqxRm8mxnQ",
+//   authDomain: "simpleiot-3dab0.firebaseapp.com",
+//   databaseURL:
+//     "https://simpleiot-3dab0-default-rtdb.asia-southeast1.firebasedatabase.app",
+//   projectId: "simpleiot-3dab0",
+//   storageBucket: "simpleiot-3dab0.appspot.com",
+//   messagingSenderId: "464866316832",
+//   appId: "1:464866316832:web:9e2ad0a42d6d477c232245",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAJh78Z3LBT9ob6dkipt64OhZqxRm8mxnQ",
-  authDomain: "simpleiot-3dab0.firebaseapp.com",
+  apiKey: "AIzaSyAbJuAUCi1cY7vg3zH_7O6vTkw-R06vIcs",
+  authDomain: "inkubator-ayam-97391.firebaseapp.com",
   databaseURL:
-    "https://simpleiot-3dab0-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "simpleiot-3dab0",
-  storageBucket: "simpleiot-3dab0.appspot.com",
-  messagingSenderId: "464866316832",
-  appId: "1:464866316832:web:9e2ad0a42d6d477c232245",
+    "https://inkubator-ayam-97391-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "inkubator-ayam-97391",
+  storageBucket: "inkubator-ayam-97391.appspot.com",
+  messagingSenderId: "955565294100",
+  appId: "1:955565294100:web:1c102234ff1a1178705d01",
 };
 
 // Initialize Firebase
